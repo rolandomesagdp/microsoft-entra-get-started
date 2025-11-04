61d6aa87215f7fe2dd5078fc3e4ad2de0c48300a
Rolando Mesa <rolandomesagdp@gmail.com>
2025-11-04 19:57:41 +0100

    Console client app included without client secret

---
+++ console-client/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Identity.Client;
+using System.Net.Http.Headers;
+
+HttpClient client = new HttpClient();
+
+var clientId = "86787070-9231-4a7b-863a-8573e65edbab";
+var clientSecret = "";
+var scope = "api://040d7919-d422-4710-84dc-66c418bd1e67/.default";
+var tenantId = "fdded263-93bc-4d52-bce1-fece3a117fb5";
+var authority = $"https://login.microsoftonline.com/{tenantId}";
+
+var app = ConfidentialClientApplicationBuilder
+    .Create(clientId)
+    .WithAuthority(authority)
+    .WithClientSecret(clientSecret)
+    .Build();
+
+var result = await app.AcquireTokenForClient(new string[] { scope }).ExecuteAsync();
+Console.WriteLine($"Access Token: {result.AccessToken}");
+
+client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", result.AccessToken);
+var response = await client.GetAsync("http://localhost:5272/api/todolist");
+var content = await response.Content.ReadAsStringAsync();
+
+Console.WriteLine($"Your response is: {response.StatusCode}");
